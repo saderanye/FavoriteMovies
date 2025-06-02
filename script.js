a0900e496a76a7243490715c4c9a2560e6d658dc
Sadera Nye <saderanye1@gmail.com>
2025-06-01 21:35:30 -0700

    append child, and array practice

---
+++ script.js
@@ -0,0 +1,33 @@
+const inputMovie = document.getElementById('inputMovie');
+const addBtn = document.getElementById('addBtn');
+const movieList = document.getElementById('movieList');
+
+
+let movies = [];
+
+addBtn.addEventListener('click', function () {
+    movies.push(inputMovie.value);
+    inputMovie.value = '';
+    renderMovies();
+})
+
+function renderMovies() {
+    movieList.innerHTML = '';
+    movies.forEach((movie, index) => {
+        const li = document.createElement('li');
+        li.textContent = movie;
+
+        const removeBtn = document.createElement('button');
+        removeBtn.textContent = 'Remove';
+
+        removeBtn.addEventListener('click', () => {
+            movies.splice(index, 1);
+            renderMovies();
+        })
+
+        li.appendChild(removeBtn);
+        
+        movieList.appendChild(li);
+    });
+};
+
